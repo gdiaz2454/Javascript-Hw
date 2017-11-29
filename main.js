6cde259f4ce99c861aad3d5ebb7294085346d206
gdiaz2454@gmail.com <gabrieldiaz@Gabriels-MacBook-Pro.local>
2017-11-29 16:08:32 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+//Variable practice
+var hot = "Jenniferlopez ";
+var rap = "Kendricklamar";
+var result1 = hot + rap
+console.log(result1)
+
+var actor = "Johnniedepp ";
+var rich = "Billgates";
+var result2 = actor + rich
+
+console.log(actor + rich)
+
+var style = "Versace ";
+var singer = "Rihanna";
+var result3 = style + singer
+console.log(style + singer)
+
+
+var sohot = prompt("Jennifer Lopez you are fine!!!")
+console.log(sohot)
+
+
+//variable math practice
+var jordan = 23;
+var kobe = 24;
+var cp = 3;
+var kmalone = 32;
+var pg = 13;
+var kyrie = 11;
+var allen = 20;
+console.log(jordan + kobe + cp + kmalone + pg + kyrie + allen)
+var sum = jordan + kobe + cp + kmalone + pg + kyrie + allen;
+var totaln = 7;
+console.log(sum / totaln)
+
+//Create function using concatented variables
+
+function displayConcats(){
+  console.log(result1,result2,result3)
+
+}
+
+displayConcats()
